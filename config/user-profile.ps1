001a669c2e467f20579caf92075e1a94467ec4df
Stig Dahl <stig@charlottendal.net>
2018-08-23 14:45:21 +0200

    Added new config with colors

--- config/user-profile.ps1
+++ config/user-profile.ps1
@@ -1,6 +1,8 @@
-# Use this file to run your own startup commands
+﻿# Use this file to run your own startup commands
 
-## Prompt Customization
+#######################################
+#         Prompt Customization
+#######################################
 <#
 .SYNTAX
     <PrePrompt><CMDER DEFAULT>
@@ -8,19 +10,115 @@
 .EXAMPLE
     <PrePrompt>N:\Documents\src\cmder [master]
     λ <PostPrompt> |
+
+.LINK
+Imported from https://github.com/AmrEldib/cmder-powerline-prompt
 #>
 
 [ScriptBlock]$PrePrompt = {
 
 }
 
+function Import-GitModule($Loaded){
+    if($Loaded) { return }
+    $GitModule = Get-Module -Name Posh-Git -ListAvailable
+    if($GitModule | Select-Object version | Where-Object version -le ([version]"0.6.1.20160330")){
+        Import-Module Posh-Git > $null
+    }
+    if(-not ($GitModule) ) {
+        Write-Warning "Missing git support, install posh-git with 'Install-Module posh-git' and restart cmder."
+    }
+    # Make sure we only run once by alawys returning true
+    return $true
+}
+
+
+
+$isGitLoaded = $false
+#Anonymice Powerline
+$arrowSymbol = [char]0xE0B0;
+$branchSymbol = [char]0xE0A0;
+
+$defaultForeColor = "White"
+$defaultBackColor = "Black"
+$pathForeColor = "White"
+$pathBackColor = "DarkBlue"
+$gitCleanForeColor = "White"
+$gitCleanBackColor = "Green"
+$gitDirtyForeColor = "Black"
+$gitDirtyBackColor = "Yellow"
+
+function Write-GitPrompt() {
+    $status = Get-GitStatus
+
+    if ($status) {
+
+        # assume git folder is clean
+        $gitBackColor = $gitCleanBackColor
+        $gitForeColor = $gitCleanForeColor
+        if ($status.HasWorking -Or $status.HasIndex) {
+            # but if it's dirty, change the back color
+            $gitBackColor = $gitDirtyBackColor
+            $gitForeColor = $gitDirtyForeColor
+        }
+
+        # Close path prompt
+        Write-Host $arrowSymbol -NoNewLine -BackgroundColor $gitBackColor -ForegroundColor $pathBackColor
+
+        # Write branch symbol and name
+        Write-Host " " $branchSymbol " " $status.Branch " " -NoNewLine -BackgroundColor $gitBackColor -ForegroundColor $gitForeColor
+
+        <# Git status info
+        HasWorking   : False
+        Branch       : master
+        AheadBy      : 0
+        Working      : {}
+        Upstream     : origin/master
+        StashCount   : 0
+        Index        : {}
+        HasIndex     : False
+        BehindBy     : 0
+        HasUntracked : False
+        GitDir       : D:\amr\SourceCode\DevDiary\.git
+        #>
+
+        # close git prompt
+        Write-Host $arrowSymbol -NoNewLine -BackgroundColor $defaultBackColor -ForegroundColor $gitBackColor
+    }
+}
+
+function getGitStatus($Path) {
+    if (Test-Path -Path (Join-Path $Path '.git') ) {
+        $isGitLoaded = Import-GitModule $isGitLoaded
+        Write-GitPrompt
+        return
+    }
+    $SplitPath = split-path $path
+    if ($SplitPath) {
+        getGitStatus($SplitPath)
+    }
+    else{
+      Write-Host $arrowSymbol -NoNewLine -ForegroundColor $pathBackColor
+    }
+}
+
+function tildaPath($Path) {
+    return $Path.replace($env:USERPROFILE, "~")
+}
+
 # Replace the cmder prompt entirely with this.
-# [ScriptBlock]$CmderPrompt = {}
+[ScriptBlock]$CmderPrompt = {
+    $tp = tildaPath($pwd.ProviderPath)
+    $tp += " `(" + "$env:computername" + "`)"
+    Microsoft.PowerShell.Utility\Write-Host "`n" $tp " " -NoNewLine -BackgroundColor $pathBackColor -ForegroundColor $pathForeColor
+
+    getGitStatus($pwd.ProviderPath)
+}
+
 
 [ScriptBlock]$PostPrompt = {
-
 }
 
 ## <Continue to add your own>
 
-. ~\Repos\powershell-stuff\aliases.ps1
+. ~/Repos/powershell-stuff/aliases.ps1
